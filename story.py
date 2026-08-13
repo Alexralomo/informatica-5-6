f0c95a6935a0d075218ab383f5b75718216bb6da
Alexralomo <manuelralomo@gmail.com>
2026-08-13 17:21:00 +0000

    Goku

---
+++ story.py
@@ -0,0 +1,19 @@
+def main():
+     Goku = input("Goku:")
+
+    # Separation
+    print("hello!", Goku)
+
+    #Ending
+    print("hello", end=" ")
+    print(goku)
+
+    # Concatenation
+    print("hello " + Goku)
+
+    #Formatted String
+    print(f"hello {Goku}")
+
+
+if __name__== "__main__":
+    main()
