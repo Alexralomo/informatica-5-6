2fae2145c0b979f5b3c8514ec553cd8ca9c269fa
Alexralomo <manuelralomo@gmail.com>
2026-08-13 20:20:07 +0000

    Sture

--- story.py
+++ story.py
@@ -1,18 +1,29 @@
 def main():
-     Goku = input("Goku:")
+    # Goku = input("Goku:")
 
-    # Separation
-    print("hello!", Goku)
+    # # Separation
+    # print("hello!", Goku)
 
-    #Ending
-    print("hello", end=" ")
-    print(goku)
+    # #Ending
+    # print("hello", end=" ")
+    # print(goku)
 
-    # Concatenation
-    print("hello " + Goku)
+    # # Concatenation
+    # print("hello " + Goku)
 
-    #Formatted String
-    print(f"hello {Goku}")
+    # #Formatted String
+    # print(f"hello {Goku}")
+
+    name= input("what is your name?").title().strip()
+    color= input("Tell me a color:").lower().strip()
+    adj=input("tell me an adjetive:")
+    goal=input("A goal you would like to achieve:")
+
+    print(f"hello {name}!")
+    print("Esta es tu historia!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    print(f" En mucho tiempo estaba un chico llamado {name}, que le encantava el color {color}, y su mallor birtud es ser {adj}, y su metas es {goal}.")
+    print(f" En mucho tiempo estaba un chico llamado {name}, que le encantava el color {color}, y su mallor birtud es ser {adj}, y su metas es {goal}.").upper()
 
 
 if __name__== "__main__":
