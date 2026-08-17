59d667d515a7d7330c8446bd4487291594a14764
Alexralomo <manuelralomo@gmail.com>
2026-08-17 19:38:29 +0000

    Rectangulo

---
+++ rectangle.py
@@ -0,0 +1,24 @@
+def main():
+
+    goku = 5
+
+
+
+
+
+
+    width = int(input("Enter the width of rectangle:"))
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+    print("0" * width)
+
+    vegeta = int(goku) + int (goku) + int (width) + int (width)
+    print("Perimetro:",vegeta)
+
+
+
+
+if __name__== "__main__":
+    main()
