357c865be2efeca0405db8f4f37fcb4279fd683e
Alexralomo <manuelralomo@gmail.com>
2026-08-18 16:40:11 +0000

    monedas

---
+++ currency.py
@@ -0,0 +1,32 @@
+def main():
+
+    print("Vamos a ver si vamos a comer con los compas de otros paises")
+    print()
+    print()
+    pesos = int(input("Cuanto ganaste en pesos mexicanos amigo?:"))
+    soles = int(input("Cuanto ganaste en pesos soles amigo?:"))
+    reais = int(input("Cuanto ganaste en pesos reais amigo?:"))
+
+    soles1 = int(soles) *5.07
+    print("De soles a pesos compa:", soles1)
+    reais1 = int(reais) *3.28
+    print("De reais a pesos compa:", reais1)
+
+    print("todo es en pesos")
+    print()
+    print()
+
+
+    total = int(soles1) + int(reais1) + int(pesos)
+    print("Juntaron:", total)
+    usa = int(total) *17.06
+    print("usa moneda:", usa)
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
--- rectangle.py
+++ rectangle.py
@@ -16,8 +16,10 @@ def main():
 
     vegeta = int(goku) + int (goku) + int (width) + int (width)
     print("Perimetro:",vegeta)
-
-
+    vegeto = int(goku) * int(width)
+    print("Area:" , vegeto)
+    pelon = (int(goku)**2 + int(width)**2)**1/2
+    print("diagonal:" , pelon)
 
 
 if __name__== "__main__":
