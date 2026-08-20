5ce14af79b9cd4a564b7abb03c5e740b212f73b6
Alexralomo <manuelralomo@gmail.com>
2026-08-20 20:11:48 +0000

    codigo

--- moores.py
+++ moores.py
@@ -1,11 +1,16 @@
 def main():
 
     year = int(input("pon los años que quieres adelantar:"))
-    transistors = 17800000000
-    transistors = transistors *year
-    print("transistors:",transistors)
+    transistors = 17.8
 
 
+    current_year = 2026
+
+    if (current_year + year) >=2030:
+    print("the law is not valid.")
+    else:
+        transistors *= 2** (year/2)
+        print("transistors:",transistors)
 
 
 
