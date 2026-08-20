805cea354f92bbbd73ddcaf879301d07e6bd46f8
Alexralomo <manuelralomo@gmail.com>
2026-08-20 17:12:21 +0000

    transistors

---
+++ moores.py
@@ -0,0 +1,15 @@
+def main():
+
+    year = int(input("pon los años que quieres adelantar:"))
+    transistors = 17800000000
+    transistors = transistors *year
+    print("transistors:",transistors)
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
+
