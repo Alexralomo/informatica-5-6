ed8763c50e8d7bd6b9ba13d086e2d0c1ac2d7d0c
Alexralomo <manuelralomo@gmail.com>
2026-08-24 19:34:35 +0000

    password

---
+++ password.py
@@ -0,0 +1,24 @@
+def main():
+
+    contra = "Mmramos1008"
+    password = input("PON TU CONTRASEÑA PORFABOR:")
+    if contra == password:
+        print("Correct Password")
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
+
+
