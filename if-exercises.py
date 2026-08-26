6049d92fd593edd6a50e1f27e7f6f67b09fcbf89
Alexralomo <manuelralomo@gmail.com>
2026-08-26 20:17:05 +0000

    calculos

---
+++ if-exercises.py
@@ -0,0 +1,13 @@
+def main():
+
+    calculo = int(input("vamos a poner numeros enteros si te tienes numeros negatibos los haremos pocitibos:"))
+
+    if calculo > 0:
+        print("Respuesta:",calculo )
+    elif calculo < 0:
+        print("esto se multiplico por -1:", calculo * -1)
+
+if __name__== "__main__":
+    main()
+
+    
