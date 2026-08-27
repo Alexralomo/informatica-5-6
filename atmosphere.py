2d28b6f7ca7bfd0ab4647fa88e09842f2a80c452
Alexralomo <manuelralomo@gmail.com>
2026-08-27 16:39:01 +0000

    exa 1

---
+++ atmosphere.py
@@ -0,0 +1,18 @@
+def main():
+
+    #variables de la admosver
+    eart_atmosfea = int(input("Quieres lansar una capsula al espacio dime a que capa lo lansas te pongo en cuanto tiempo llrga:"))
+
+
+    if eart_atmosfea == troposphere:
+        print("ufas se encuentra en el rango de (0–12 km) con un tiempo de:",12/20)
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
