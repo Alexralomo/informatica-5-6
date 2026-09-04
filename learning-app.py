0360cd4f3e60777668589411254f79f1256e86fb
Alexralomo <manuelralomo@gmail.com>
2026-09-04 17:30:21 +0000

    s

---
+++ learning-app.py
@@ -0,0 +1,29 @@
+import random
+def main():
+    print("hola amigo bienbenido a mi aplicacion, aprenderemos a sumar y te iremos preparando para usarlas diarias mente")
+    print()
+    print()
+    num1 = random.randint(1,100)
+    num2 = random.randint(1,100)
+    num3 = num1 + num2
+
+    num4 = print("suma los sigientes numeros", num1,"+", num2)
+    num5 = int(input("escrive tu respuesta:"))
+    if num5 == num3:
+        print("correctooooooooooooooooooooooooooooo")
+    elif nom5 != num3:
+        print("no no amigo")
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
