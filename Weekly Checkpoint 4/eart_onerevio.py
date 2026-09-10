5d283a9670418d54a3deda416225a5ef9f9af1d8
Alexralomo <manuelralomo@gmail.com>
2026-09-10 17:26:56 +0000

    days

--- Weekly Checkpoint 4/eart_onerevio.py
+++ Weekly Checkpoint 4/eart_onerevio.py
@@ -1,7 +1,20 @@
+from datetime import datetime
 def main():
 
+    day = datetime.now().weekday()
+    days = ["Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday", "sunday"]
+    print(days[day])
+    print(day)
+    months = ["January", "February", "March", "Abril", "May", "June", "July", "August", "September", "October", "November", "December"]
+    print("These are the summer months:")
+    print(months[5])
+    print(months[6])
+    print(months[7])
+
+
     print("What day is it today?")
-    day = int(input())
+
+
 
     if day < 4:
         print("It's a weekday")
@@ -10,18 +23,8 @@ def main():
     elif day == 4:
         print("it's Friday")
         print("just a day left until the weekend")
-
-
-
-
     else:
         print("It's the weekend!!!")
-
-
-
-
-
-
 
 
 
