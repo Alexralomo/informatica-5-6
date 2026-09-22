2613d9ec3bc7af04ef3c920eafcaf3dcec61765c
Alexralomo <manuelralomo@gmail.com>
2026-09-22 16:39:35 +0000

    carta

---
+++ letter.py
@@ -0,0 +1,33 @@
+def main():
+
+    personas = ["Mario", "Luigi", "Daisy", "Yoshi", "Peach", "Bowser"]
+    inbitando = "Princess Peach"
+    for reciver in personas:
+        if receiver != "Peach":
+            print(f"""
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+    para {receiver},
+
+    Hola te estoy inbitando a mi fiesta de recate en el castillo de Boswer
+    hay mucha comida y juegos te espero
+
+        de:
+        {personas[5]}
+
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+    print""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
