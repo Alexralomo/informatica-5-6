40a280ba30d78fd13b038b9d1685dcf9b66dc62e
Alexralomo <manuelralomo@gmail.com>
2026-09-23 20:20:01 +0000

    tablas

---
+++  tables.py
@@ -0,0 +1,19 @@
+def main():
+     tablas_multa = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+     tablas = input(" Cual tabla quieres aprender tenemos del 1 al 10")
+
+        for tablas in tablas_multa:
+            if tabla != "":
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
